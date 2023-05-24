c4d4c587ddcd0fb677b8fd6f62207e52ddd47243
Matheus <matheus7rod@gmail.com>
2023-05-24 17:04:35 -0300

    feat: add alice package to handle middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
@@ -13,5 +17,7 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standardMiddleware.Then(mux)
 }
