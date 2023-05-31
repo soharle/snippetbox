03f0571f4804b7e102c621ad92b76956455fd4d9
Matheus <matheus7rod@gmail.com>
2023-05-31 00:02:46 -0300

    tests: add some unit test

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -32,6 +32,10 @@ type userLoginForm struct {
 	validator.Validator `form:"-"`
 }
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	snippets, err := app.snippets.Latest()
 	if err != nil {
